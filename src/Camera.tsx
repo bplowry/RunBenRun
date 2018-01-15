643313f3206f9bf63fcf308e3dbde4293b498f18
Ben Lowry <ben.lowry@readify.net>
2018-01-15 13:48:42 +0800

    add typings for expo and use expokit for camera/file interaction to get around needing to eject

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import * as Expo from 'expo';
+
 import { View, Button, Image, Alert, ScrollView, PermissionsAndroid, CameraRoll, GetPhotosReturnType } from 'react-native';
 import { HttpExecutor } from './Shared/HttpExecutor';
 import { BodyText } from './Components/TextComponents';
@@ -7,74 +9,44 @@ import { Progress } from './Components/Progress';
 export class Camera extends React.Component<{
     navigation: any,
 }, {
-    canUseCamera: boolean | null;
-    photos: GetPhotosReturnType | null;
+    image: { height: number, width: number, uri: string; } | null;
 }> {
     constructor(props) {
         super(props);
 
-        this.state = {
-            canUseCamera: null,
-            photos: null,
-        };
+        this.state = { image: null };
     }
 
-    async checkPermissions() {
-        let canUseCamera = await PermissionsAndroid.check('android.permission.CAMERA');
-        if (!canUseCamera)
-            canUseCamera = await PermissionsAndroid.request('android.permission.CAMERA', {
-                title: 'Requesting access to use the camera',
-                message: 'We need access to the camera to allow you to take and instantly upload photos',
-            }) === 'granted';
-
-        const storage = PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE; // 'android.permission.READ_EXTERNAL_STORAGE';
-        let canReadStorage = await PermissionsAndroid.check(storage);
-        if (!canReadStorage)
-            canReadStorage = await PermissionsAndroid.request(storage, {
-                title: 'Requesting access to storage',
-                message: 'We need access to storage so you can select photos/videos to upload',
-            }) === 'granted';
-
-        await this.setState({ canUseCamera });
-    }
-
-    async getPhotos() {
-        if (!CameraRoll || !CameraRoll.getPhotos) {
-            console.warn('CameraRoll not a thing');
-            return;
+    async takePicture() {
+        const res = await Expo.ImagePicker.launchCameraAsync();
+        if (!res.cancelled) {
+            this.setState({image: res});
         }
+    }
 
-        const photos = await CameraRoll.getPhotos({
-            assetType: 'All',
-            first: 2,
+    async getPhoto() {
+        const res = await Expo.ImagePicker.launchImageLibraryAsync({
+            mediaTypes: 'All',
         });
 
-        await this.setState({ photos });
+        if (!res.cancelled) {
+            this.setState({image: res});
+        }
     }
 
     render() {
-        const { canUseCamera, photos } = this.state;
+        const img = this.state.image;
         return (
             <View>
-                <Button title='Check permissions' onPress={() => this.checkPermissions()} />
-                { canUseCamera === null
-                ? <BodyText>Checking permissions ...</BodyText>
-                : canUseCamera
-                    ? <Button title='Get photos' onPress={() => this.getPhotos()} />
-                    : <BodyText>Permission to use camera has been denied.</BodyText> }
-                <View>
-                    { photos && photos.edges && photos.edges.map((p, i) => {
-                        const image = p.node.image;
-                        return <Image
-                            key={i}
-                            source={{ uri: image.uri}}
-                            style={{
-                                height: image.height,
-                                width: image.width,
-                            }}
-                        />;
-                    }) }
-                </View>
+                <Button title='Take photo' onPress={() => this.takePicture()} />
+                <Button title='Select image' onPress={() => this.getPhoto()} />
+                { img && <Image
+                    style={{
+                        height: 200,
+                        width:  300,
+                    }}
+                    source={{ uri:  img.uri }}
+                />}
             </View>
         );
     }
